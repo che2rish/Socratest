b1a0733bec7c52566bd8f3e4d1104deb75b064f8
che2rish <whdkfka101@naver.com>
2022-01-16 22:22:52 +0900

    추가 : 입력값 전달

--- static/js/test.js
+++ static/js/test.js
@@ -2,25 +2,32 @@ const endPoint = 5;
 let qidx = 0;
 
 function goNextQ(){
-  // if (document.getElementById("answer").value==""){
-  //   alert('원활한 테스트를 위해서 대답을 입력해주세요 :)')
-  // }else{
-  //   goNext(++qidx);
-  // }
-  goNext(++qidx);
+   if (document.getElementById("answer").value==""){
+     alert('원활한 테스트를 위해서 대답을 입력해주세요 :)')
+   }else{
+     goNext(++qidx);
+   }
+  //goNext(++qidx);
 }
 
 function goNext(qidx){
+  var answer = $('#answer').val();
+  console.log(answer);
+  sessionStorage.setItem(qidx, answer);
+
   if(qidx === 5){
     move_result();
-  }
+    result();
+  }else{
   var q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qidx].q;
   qidx++;
   console.log(qidx);
+
   nextImg(qidx);
   nextText();
-  
+  }
+
   var status = document.querySelector('.statusBar');
   status.style.width = (100/endPoint) * (qidx)+ '%';
 }
@@ -28,7 +35,7 @@ function goNext(qidx){
 function nextImg(qidx){
   // var Q_Img = document.createElement('img');
   var imgDiv = document.querySelector('#Q_Img');
-  var imgURL = '../static/img/Q' + qidx + ".png"; 
+  var imgURL = '../static/img/Q' + qidx + ".png";
 
   // Q_Img.src = imgURL;
   // Q_Img.alt = qidx;
@@ -41,4 +48,29 @@ function nextImg(qidx){
 function nextText(){
   var textBox = document.querySelector('.answer');
   textBox.innerHTML = "<input type='text' id = 'answer' name='answer' class='col-lg-6 col-md-8 col-sm-10 col-12 my-3 mx-auto'style='font-size: 26px;'></input>"
+}
+
+// 결과 전송
+function result(){
+var obj = {};
+
+for(i=1; i<=5; i++){
+    obj[i] = sessionStorage.getItem(i);
+}
+
+        $.ajax({
+            type: 'POST'
+            ,url : 'http://192.168.1.113:5000/ajax' //데이터를 주고받을 파일 주소 입력,''
+            ,data: JSON.stringify(obj)
+            ,dataType: 'JSON'
+            ,contentType: "application/json"
+            ,success: function(code){
+                //$("#tempLbl").text(code.result['SEND_DATA'] + " " + code.result['SEND_DATA2']);
+                //console.log(obj);
+                alert(obj);
+            }
+            ,error:function(){
+                alert("error");
+            }
+        });
 }
